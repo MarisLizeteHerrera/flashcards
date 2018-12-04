f723811b8e25cdff77a5b16f9d90f97ae8895328
Maris Herrera <maris.lizete.herrera@gmail.com>
2018-12-03 17:42:52 -0700

    init commit

---
+++ flashcards.js
@@ -0,0 +1,34 @@
+// document.addEventListener('DOMcontentLoaded', () => {
+//   //Add new card button
+//   let addCard = document.querySelector('.add-card');
+//   addCard.addEventListener('click', () => {
+//     console.log('Clicked!');
+//   });
+// });
+var title = document.getElementById('title')
+var question = document.getElementById('question')
+var answer = document.getElementById('answer')
+
+
+function addcard(title = [], question = [], answer = []) {
+  card = {title, question, answer}
+
+}
+
+addCard.addEventListener('click', () => {
+  console.log('Clicked!');
+
+
+
+function ans() {
+  
+  console.log('Clicked!')
+}
+
+
+
+// const hello = (greeting) => {
+//   console.log(greeting);
+// }
+
+// hello("Sup!");
